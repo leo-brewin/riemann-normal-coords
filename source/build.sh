74e8e6a40f247875e5626d14f4c316b9e22192f9
Leo Brewin <Leo.Brewin@monash.edu>
2019-12-20 13:08:07 +1100

    Updated to match version on arxiv
    Updated to match version on arxiv.

--- source/build.sh
+++ source/build.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-(cd cadabra; make github)
-(cd tex; make github)
-
 mkdir -p ../pdf
 
-cp -rf tex/paper.pdf                           ../pdf/paper.pdf
-cp -rf cadabra/pdf/summary.pdf                 ../pdf/summary.pdf
-cp -rf cadabra/pdf/collection.pdf              ../pdf/collection.pdf
+(cd cadabra; make github)
+(cd tex; make github)
